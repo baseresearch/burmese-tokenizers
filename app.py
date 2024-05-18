b1e9d2d1e2044242444c24403d53845bbe6446a0
swanhtet1992 <swanhtet1992@gmail.com>
2024-05-17 23:54:19 -0400

    Update dataset

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import random, glob
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("MassiveDatasetValidationData.csv")
+    return pd.read_csv("dataset.csv")
 
 
 def reload_example_text_data(language):
